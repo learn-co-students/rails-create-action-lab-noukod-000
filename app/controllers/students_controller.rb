5ce095b867b9658d7b909bdfb7387978190e7b5f
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-27 12:54:47 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(first_name: params[:first_name], last_name: params[:last_name])
+    @student = Student.new#(first_name: params[:first_name], last_name: params[:last_name])
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     @student.save
